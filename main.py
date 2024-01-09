445a38623fc5fda1a33e6d0ecddd45ebd1b98f10
Jayhawk <89945681+aussiejayhawk@users.noreply.github.com>
2024-01-09 20:15:36 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# import os file open.
+
+# import excel module
+
+
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Hello, Tkinter")
+
+greeting.pack()
+
+window.mainloop()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
