4178eafc65ec0ca25e589e252aac417b48eb5eee
Jayhawk <89945681+aussiejayhawk@users.noreply.github.com>
2024-01-10 15:28:33 +1000

    setting up gui and file input

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ import tkinter as tk
 window = tk.Tk()
 
 greeting = tk.Label(text="Please Select your workbook file")
-root.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+
+button = tk.Button(
+    text="Browse...",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+#root.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
 
 
 greeting.pack()
